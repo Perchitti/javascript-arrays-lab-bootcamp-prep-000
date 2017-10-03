7871d6f64ebc0b50be1e64a7470bd08e33edf5e3
Lauren Perchitti <laurenperchitti@gmail.com>
2017-10-03 19:45:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,10 +30,10 @@ function prependKitten(name){
   return [name, ...kittens]
 }
 
-function removeLastKitten(array){
+function removeLastKitten(){
   return kittens.slice(1)
 }
 
-function removeFirstKitten(array){
+function removeFirstKitten(){
   return kittens.slice(0, kittens.length - 1)
 }
