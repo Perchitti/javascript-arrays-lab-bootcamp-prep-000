8ef47aebc4b47847ae0b9fcdddaa8d101dd7558f
Lauren Perchitti <laurenperchitti@gmail.com>
2017-10-03 19:32:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,25 @@ const app = "I don't do much."
 var kittens = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(array, element){
- array.push("Ralph")
+ kittens.push("Ralph")
 
  return array
+}
+
+function destructivelyPrependKitten(array, element){
+  kittens.unshift("Bob")
+
+  return array
+}
+
+function destructivelyRemoveLastKitten(array){
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(array){
+  kittens.shift()
+}
+
+function appendKitten(array, name){
+  return [array, ..."Broom"]
 }
